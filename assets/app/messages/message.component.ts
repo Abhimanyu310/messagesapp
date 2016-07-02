8bd01806a0b8f00e1b68ff84e12cc6f106b2f432
Abhimanyu310 <abhip310@gmail.com>
2016-07-02 19:46:47 +0530

    Added delete feature

--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -51,6 +51,10 @@ export class MessageComponent {
     }
 
     onDelete(){
-        this._messageService.deleteMessage(this.message);
+        this._messageService.deleteMessage(this.message)
+            .subscribe(
+                data => console.log(data),
+                error => console.error(error)
+            );
     }
 }
--- routes/messages.js
+++ routes/messages.js
@@ -68,6 +68,35 @@ router.patch('/:id', function (req, res, next) {
     });
 })
 
+router.delete('/:id', function (req, res, next) {
+    Message.findById(req.params.id, function (err, doc) {
+        if(err){
+            return res.status(404).json({
+                title: 'An error occured',
+                error: err
+            });
+        }
+        if(!doc){
+            return res.status(404).json({
+                title: 'No message found',
+                error: {message: 'Message could not be found'}
+            });
+        }
+        doc.remove(function (err, result){
+            if(err){
+                return res.status(404).json({
+                    title: 'An error occured',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Success',
+                obj: result
+            });
+        });
+    });
+});
+
 
 module.exports = router;
 
